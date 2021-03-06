4aa006c2a13638fcaf1f6eef5aa91808586a816f
Tasso Moraes <tassoluis@gmail.com>
2021-03-06 16:32:20 -0300

    AI App Layout finished

--- AprendaIngles/app/src/main/java/com/tlom/aprendaingles/MainActivity.java
+++ AprendaIngles/app/src/main/java/com/tlom/aprendaingles/MainActivity.java
@@ -9,6 +9,7 @@ import com.ogaclejapan.smarttablayout.SmartTabLayout;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 import com.tlom.aprendaingles.Fragments.BichosFragment;
+import com.tlom.aprendaingles.Fragments.NumerosFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,13 +24,20 @@ public class MainActivity extends AppCompatActivity {
         smartTabLayout = findViewById(R.id.viewPagerTab);
         viewPager = findViewById(R.id.viewPager);
 
+        //Ocultando a sobra da ActionBar
+        getSupportActionBar().setElevation(0);
+
         //Definido adapter da SmartTab
         FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
                 getSupportFragmentManager(),
                 FragmentPagerItems.with(this)
                         .add("Bichos", BichosFragment.class)
+                        .add("Números", NumerosFragment.class)
                         .create()
         );
+
+        viewPager.setAdapter( adapter );
+        smartTabLayout.setViewPager( viewPager );
 
     }
 }
