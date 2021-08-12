67bea8696244bf32a5ce8ede8b11aaf285f50827
Tasso Moraes <tassoluis@gmail.com>
2021-08-12 16:38:00 -0300

    Flappy Bird Game

---
+++ FlappyBird/core/src/com/tlom/flappybird/FlappyBird.java
@@ -0,0 +1,24 @@
+package com.tlom.flappybird;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class FlappyBird extends ApplicationAdapter {
+
+	
+	
+	@Override
+	public void create () {
+
+	}
+
+	// Para as animações
+	@Override
+	public void render () {
+
+	}
+
+}
