c4c009ec9136893784a5468b890b9b151637a96c
Tasso Moraes <tassoluis@gmail.com>
2021-08-12 16:59:56 -0300

    Background and bird moviment

--- FlappyBird/core/src/com/tlom/flappybird/FlappyBird.java
+++ FlappyBird/core/src/com/tlom/flappybird/FlappyBird.java
@@ -8,16 +8,44 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class FlappyBird extends ApplicationAdapter {
 
-	
-	
+	private SpriteBatch batch; 	// classe para criar animações
+	private Texture passaro;
+	private Texture fundo;	// plano de fundo
+
+	// Atributos de configuração
+	private int movimento = 0;
+	private int largura_tela;
+	private int altura_tela
+
+	// Para inicializar o jogo
 	@Override
 	public void create () {
+
+		batch = new SpriteBatch();
+		passaro = new Texture("passaro1.png");
+		fundo = new Texture("fundo.png");
+
+		// getWidth recupera a largura da tela e getHight peda a altura
+		largura_tela = Gdx.graphics.getWidth();
+		altura_tela = Gdx.graphics.getHeight();
 
 	}
 
 	// Para as animações
 	@Override
 	public void render () {
+
+		// para o passaro se mover na tela
+		movimento++;
+
+		// para exibir alguma textura
+		batch.begin();
+
+		batch.draw(fundo, 0,0, largura_tela, altura_tela);
+		// colocar textura passaro na posição x,y
+		batch.draw(passaro, movimento, 400);
+
+		batch.end();
 
 	}
 
