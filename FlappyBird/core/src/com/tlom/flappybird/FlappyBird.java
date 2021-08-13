8d2c623f8518ca7402c80f4eaf25ac13065b2def
Tasso Moraes <tassoluis@gmail.com>
2021-08-13 13:52:41 -0300

    Click event

--- FlappyBird/core/src/com/tlom/flappybird/FlappyBird.java
+++ FlappyBird/core/src/com/tlom/flappybird/FlappyBird.java
@@ -11,14 +11,17 @@ public class FlappyBird extends ApplicationAdapter {
 	private SpriteBatch batch; 	// classe para criar animações
 	private Texture[] passaros;
 	private Texture fundo;	// plano de fundo
+	private Texture cano_baixo;
+	private Texture cano_topo;
 
 	// Atributos de configuração
 	private int largura_tela;
 	private int altura_tela;
 
 	private float variacao = 0;
-	private int velocidade_queda = 0;
-	private int posicao_inicial_vertical;
+	private float velocidade_queda = 0;
+	private float posicao_inicial_vertical;
+	private float posicao_cano_movimento_horizontal;
 
 	// Para inicializar o jogo
 	@Override
@@ -29,13 +32,18 @@ public class FlappyBird extends ApplicationAdapter {
 		passaros[0] = new Texture("passaro1.png");
 		passaros[1] = new Texture("passaro2.png");
 		passaros[2] = new Texture("passaro3.png");
+
 		fundo = new Texture("fundo.png");
+		cano_baixo = new Texture("cano_baixo.png");
+		cano_topo = new Texture("cano_topo.png");
 
 		// getWidth recupera a largura da tela e getHight peda a altura
 		largura_tela = Gdx.graphics.getWidth();
 		altura_tela = Gdx.graphics.getHeight();
 
 		posicao_inicial_vertical = altura_tela / 2;
+
+		posicao_cano_movimento_horizontal = largura_tela;
 
 	}
 
@@ -49,7 +57,12 @@ public class FlappyBird extends ApplicationAdapter {
 
 		if(variacao > 2) variacao = 0;
 
-		if(posicao_inicial_vertical > 0)
+		// evento de click
+		if( Gdx.input.justTouched() ){
+			velocidade_queda = -15;
+		}
+
+		if(posicao_inicial_vertical > 0 || posicao_inicial_vertical < 0)
 			posicao_inicial_vertical -= velocidade_queda;
 
 		// para exibir alguma textura
